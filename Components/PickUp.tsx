ef610cc905bb6fe07ee043f11e3c5693beadd63e
whoisxnio <enioblvck@gmail.com>
2023-03-30 00:49:59 +0100

    neaby components done

--- Components/PickUp.tsx
+++ Components/PickUp.tsx
@@ -1,20 +1,67 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { Broadcast } from 'phosphor-react-native'
 import React from 'react'
 
 const PickUp = () => {
   return (
-    <View style={styles.container}>
-      <Text>PickUp</Text>
+    <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+      <View style={styles.banner}>
+
+      </View>
+
+      <View style={styles.nearby}>
+        <Text style={styles.nearbyText}>Nearby collectors</Text>
+        <View style={styles.nearbyValue}>
+            <Text style={styles.nearbyTextValue}>30 km</Text>
+            <Broadcast color='#157DC8' size={16} />
+        </View>
+      </View>
+
+      <View>
+
+      </View>
     </View>
+    </ScrollView>
+    
   )
 }
 
 
 const styles = StyleSheet.create({
+    scrollView: {
+        paddingTop: 10,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    banner: {
+        width: '90%',
+        height: 250,
+        borderRadius: 8,
+        backgroundColor: '#6AB891',
+    },
+    nearby: {
+        width: '100%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 20,
+    },
+    nearbyValue: {
+        width: 60,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    nearbyText: {
+        fontSize: 16,
+        fontWeight: '800'
+    },
+    nearbyTextValue: {
+        color: '#157DC8',
+        fontSize: 12
     }
 })
 
